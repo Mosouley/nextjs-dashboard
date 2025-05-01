972fadfb2a45efa546fd05d0fc2802b465c6b088
mosouley <mo.souley@gmail.com>
2025-05-01 09:26:13 +0300

    pushing on git

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,6 @@
+export default function Page() {    
+    return  <p>
+            <h1>Welcome to the Dashboard</h1>
+        </p>
+    
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+
+
+export default function Page() {    
+    return  <p>
+            <h1>Welcome to the Dashboard</h1>
+        </p>
+    
+}
